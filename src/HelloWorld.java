ac4f517be32cbe6c412855e14afbe3794164ce01
Kwang-min-lee1 <minwinn1@gmail.com>
2024-02-06 13:50:35 +0900

    첫번째 커밋, 자바 첫 실행, 주석 및 Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+// 주석 : 프로그램과 상관없이 코드에 붙인 설명
+// 한줄 주석 : 단축키는 Ctrl + /
+/*
+여러줄 주석
+작성일 : 2024 -02- 06
+작성자 : 이광민
+ */
+ public class HelloWorld { //클래스 선언부
+    public static void main(String[] args){ // 매소드 선언부
+        // main 매소드는 진입점 entry point
+        System.out.println("hello,world!//문자열에는 주석처리가 안됨"); //콘솔에 출력
+    }
+}
---
+++ src/Statement.java
@@ -0,0 +1,16 @@
+public class Statement {
+    public static void main(String[] args) {
+        // 실행문 끝에는 반드시 세미콜론을 붙여서 실행문이 끝났음을 표시해야 한다.
+        //System.out.println("Hello")
+
+        //실행문의 종류
+        int x;  // x라는 변수를 선언
+        x = 1; // x에 1을 저장해라.
+        int y = 2; //y를 선언하고 2를 저장해라
+        int result = x + y; // result를 선언하고 x 와 y를 더해서 저장해라
+        System.out.println((result); //츨력해라
+
+
+    }
+}
+in
