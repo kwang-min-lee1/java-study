d6723f073dd04b3eab8a2b51613bdd7c14107d17
Kwang-min-lee1 <minwinn1@gmail.com>
2024-02-15 12:47:35 +0900

    메서드 기초

--- src/method/Ex1.java
+++ src/method/Ex1.java
@@ -3,6 +3,12 @@ package method;
 public class Ex1 {
     public static void main(String[] args) {
         // 두 수를 입력받아 뻴셈을 하는 메서드를 만들어주세요.
+        int num = sub(10,5);
+        System.out.println(num);
+        System.out.println(sub(10,5));
+    }
 
+    public static int sub(int a ,int b) {
+        return a - b;  // 변수에 담을 필요가 없을 경우 inline으로 리턴
     }
 }
--- src/method/Ex2.java
+++ src/method/Ex2.java
@@ -3,6 +3,29 @@ package method;
 public class Ex2 {
     public static void main(String[] args) {
         // 세 수를 입력받아 합계와 평균을 구하는 메소드를 만들어주세요.
+        sumAndAverage(10,20,30);
+        sumAndAverage(10.5, 20.7, 30.9);
+        sumAndAverage(32140978121L, 23234213, 1232190);
     }
+
+    // 메소드 오버로딩
+    public static void sumAndAverage(int x , int y , int z) {
+        int sum = x +y + z;
+        double avg = sum / 3.0;
+        System.out.println("합계 : " + sum + " 평균 : " + avg);
+    }
+
+    public static void sumAndAverage(double x , double y , double z) {
+        double sum = x +y + z;
+        double avg = sum / 3.0;
+        System.out.println("합계 : " + sum + " 평균 : " + avg);
+    }
+
+    public static void sumAndAverage(long x , long y , long z) {
+        long sum = x +y + z;
+        double avg = sum / 3.0;
+        System.out.println("합계 : " + sum + " 평균 : " + avg);
+    }
+
 
 }
