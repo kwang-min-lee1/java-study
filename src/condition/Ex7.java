3de4c9fd48ce189b2190c455d3ec17dd3e1f1766
Kwang-min-lee1 <minwinn1@gmail.com>
2024-03-08 16:02:16 +0900

    Condition 삼항연산자를 사용해서 짝수와 홀수를 구별하는 프로그램 만들기

--- src/condition/Ex7.java
+++ src/condition/Ex7.java
@@ -1,10 +1,19 @@
-//package condition;
-//
-//public class Ex7 {
-//    public static void main(String[] args) {
-//        /*
-//         삼항연산자를 사용해서 짝수와 홀수를 구별하는 프로그램을 작성해주세요.
-//         Hint : num % 2
-//         */
-//    }
-//}
+package condition;
+
+
+import java.util.Scanner;
+
+public class Ex7 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자연수 숫자를 입력해 주세요: ");
+        int input = scanner.nextInt();
+
+        System.out.println(input % 2 == 0 ? "짝수" : "홀수");
+        /*
+         삼항연산자를 사용해서 짝수와 홀수를 구별하는 프로그램을 작성해주세요.
+         Hint : num % 2
+         */
+    }
+}
