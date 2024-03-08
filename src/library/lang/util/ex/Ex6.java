e29585b186f806fcf63e8f0e73eb2ac0ebe2abeb
Kwang-min-lee1 <minwinn1@gmail.com>
2024-03-08 15:39:50 +0900

    회의 시간대 변환하기 연습문제

--- src/library/lang/util/ex/Ex6.java
+++ src/library/lang/util/ex/Ex6.java
@@ -11,12 +11,15 @@ public class Ex6 {
     연습 문제 6: 회의 시간대 변환하기
     요구사항: 뉴욕에서 오후 3시에 시작하는 온라인 회의가 있다고 할 때,
      이 회의가 서울과 런던에서 몇 시에 시작하는지 계산하여 출력하세요.
-    ZonedDateTime과 ZoneId를 사용하여 시간대 변환을 수행하세요.
+    ZonedDateTime 과 ZoneId를 사용하여 시간대 변환을 수행하세요.
 
     출력예시:
     회의는 뉴욕 기준 오후 3시에 시작합니다.
     서울에서는 회의가 오전 5시에 시작합니다.
     런던에서는 회의가 오후 8시에 시작합니다.
+
+    * 참고:  DateTimeFormatter 클래스
+    https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/time/format/DateTimeFormatter.html
 
      */
  public static void main(String[] args) {
@@ -28,7 +31,7 @@ public class Ex6 {
      System.out.println("회의는 뉴욕 기준으로 오후 3시에 시작합니다.");
 
      // 날짜 형식에 패턴 문자를 맞춘다. a(AM/PM) h(시간)
-     DateTimeFormatter fmt = DateTimeFormatter.ofPattern("a h시");
+     DateTimeFormatter fmt = DateTimeFormatter.ofPattern("a h시"); // 출력 포멧
 
      // 2. 서울 시간
      ZoneId SeoulZoneId = ZoneId.of("Asia/Seoul");  // withZoneSameInstant 해당 시간대와 동일한 다른 지역의 시간을 리턴하는 메서드
@@ -43,6 +46,6 @@ public class Ex6 {
 
      String londonFormattedTime = londonZonedDateTime.format(fmt);
      System.out.printf("런던에서는 회의가 %s에 시작합니다.\n", londonFormattedTime);
-
  }
+
 }
