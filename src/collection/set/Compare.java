498d66e7d75dee6680fe0e5e13ad39bc9320442f
Kwang-min-lee1 <minwinn1@gmail.com>
2024-03-13 16:33:21 +0900

    set compare

---
+++ src/collection/set/Compare.java
@@ -0,0 +1,64 @@
+package collection.set;
+
+import java.util.TreeSet;
+
+public class Compare {
+    // Comparable 인터페이스
+    // java.lang 패키지
+    // comparable  을 구현하는 클래스의 객체들은 자연스러운 순서로 정렬될 수 있음.
+    // 단 하나의 추상 메서드
+    // compareTo (다른 객체)
+    // 반환값 (정수)
+    // 음수: 이 객체가 저 객체보다 작다
+    // 0: 이 객체와 저 객체는 같다.
+    // 양수: 이 객체가 저 객체보다 크다.
+
+    static class Person implements Comparable<Person>{
+        String name;
+        int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public int compareTo(Person otherPerson) {
+            // 나이에 따라서 정렬하고 싶음. (양수(쿰), 0(같음), 음수(작음))
+            return this.age - otherPerson.age;
+        }
+
+        @Override
+        public String toString() {
+            return name + ":" + age;
+        }
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("홍길동", 30);
+        Person person2 = new Person("임꺽정", 40);
+        Person person3 = new Person("전우치", 20);
+        Person person4 = new Person("이몽룡", 30);
+
+        // compareTo 메서드에 따라 비교기준 음수, 0, 양수 반환
+        int compareInt12 = person1.compareTo(person2);
+        int compareInt13 = person1.compareTo(person3);
+        int compareInt14 = person1.compareTo(person4);
+
+        System.out.println("compareInt12 = " + compareInt12);
+        System.out.println("compareInt13 = " + compareInt13);
+        System.out.println("compareInt14 = " + compareInt14);
+
+        // compareTo 기준으로 자동 정렬
+        TreeSet<Person> personTreeSet = new TreeSet<>();
+        personTreeSet.add(person1);
+        personTreeSet.add(person2);
+        personTreeSet.add(person3);
+        personTreeSet.add(person4);
+
+        // 요소 출력으로 확인  -> age 로 판단하여, 홍길동과 이몽룡은 같은 요소로 인식, 이몽룡은 출력되지 않는다.
+        for (Person person : personTreeSet) {
+            System.out.println(person);
+        }
+    }
+}
---
+++ src/collection/set/Compare2.java
@@ -0,0 +1,57 @@
+package collection.set;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class Compare2 {
+    // Comparator 인터페이스
+    static class Person {
+        String name;
+        int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return name + ":" + age;
+        }
+    }
+
+    public static void main(String[] args) {
+        // 비교자 Comparator 정의: 익명 객체로 인터페이스 본문 선언
+        Comparator<Person> nameComparator = new Comparator<>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                // 나이비교
+                // return o1.age -o2.age;
+                // 이름 비교: 문자열(Comparable) 필드의 비교 방법을 사용하여 비교
+                return o1.name.compareTo(o2.name);
+            }
+
+        };
+
+        // 이름 비교자를 사용한 정렬
+        TreeSet<Person> treeSet = new TreeSet<>(nameComparator);
+
+        Person person1 = new Person("홍길동", 30);
+        Person person2 = new Person("임꺽정", 40);
+        Person person3 = new Person("전우치", 20);
+        Person person4 = new Person("이몽룡", 30);
+
+        treeSet.add(person1);
+        treeSet.add(person2);
+        treeSet.add(person3);
+        treeSet.add(person4);
+
+        //  홍길동과 이몽룡은 나이가 같지만,이름으로 판단하여 이몽룡이 출력가능
+        for (Person person : treeSet) {
+            System.out.println(person);
+        }
+
+
+
+    }
+}
