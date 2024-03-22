95cc79a1268a833bb969d6a98040387307553736
Kwang-min-lee1 <minwinn1@gmail.com>
2024-03-22 16:09:29 +0900

    연습문제

--- src/quiz/quiz5/q4/Q4.java
+++ src/quiz/quiz5/q4/Q4.java
@@ -28,30 +28,41 @@ import java.util.*;
  */
 public class Q4 {
     public static void main(String[] args) {
-        List<String> bagItems = new ArrayList<>(Arrays.asList("지갑", "여권", "선글라스", "책", "삼각대", "충전기", "지도", "겨울옷"));
 
+        // 문자열을 저장하는 ArrayList bagItems 를 생성하고, 초기 값을 주어 초기 물품을 추가
+        List<String> bagItems = new ArrayList<>(Arrays.asList("지갑", "여권", "선글라스", "책", "삼각대", "충전기", "지도", "겨울옷"));
+        // 원본 물품 리스트를 출력
         System.out.println("원본 물품 리스트: " + bagItems);
 
+        // Collections 클래스의 sort 메서드를 사용하여 물품 리스트를 알파벳 순으로 정렬
         Collections.sort(bagItems);
         System.out.println("정렬 후 물품 리스트 " + bagItems);
 
+        // Collections 클래스의 reverse 메서드를 사용하여 물품 리스트의 순서를 뒤집기
         Collections.reverse(bagItems);
         System.out.println("뒤집은 후 물품 리스트 " + bagItems);
 
+        // Collections 클래스의 shuffle 메서드를 사용하여 물품 리스트의 순서를 무작위로 섞기
         Collections.shuffle(bagItems);
         System.out.println("셔플 후 물품 리스트 " + bagItems);
 
+        // 추가할 물품을 나타내는 문자열 리스트를 생성
         List<String> additionItems = Arrays.asList("휴대폰", "카메라", "책");
+        // addAll 메서드를 사용하여 추가할 물품 리스트를 기존 물품 리스트에 추가
         bagItems.addAll(additionItems);
         System.out.println("추가 물품 후 리스트 " + bagItems);
 
+        // 빈도를 계산할 대상 물품을 나타내는 문자열 변수를 선언하고 초기화
         String targetItem = "책";
+        // Collections 클래스의 frequency 메서드를 사용하여 대상 물품의 빈도수를 계산
         int frequency = Collections.frequency(bagItems,targetItem);
         System.out.println("'" + targetItem + "'의 빈도수: "  + frequency);
 
+        // 중복된 물품을 제거하기 위해 LinkedHashSet 을 사용하여 새로운 Set 컬렉션을 생성
         Set<String> uniqueItems = new LinkedHashSet<>(bagItems);
         System.out.println("중복 제거 후 리스트" + uniqueItems);
 
+        // Collections 클래스의 min 메서드를 사용하여 물품 리스트에서 가장 작은 값을 찾기
         String mostImportantItem = Collections.min(bagItems);
         System.out.println("가장 중요한 물품(최소값): " + mostImportantItem);
 
