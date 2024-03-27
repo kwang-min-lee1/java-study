917b822015bbda372f1bcad3b18bfc67f032cd38
Kwang-min-lee1 <minwinn1@gmail.com>
2024-03-27 12:20:05 +0900

    PrintStream 객체의 직렬화, 역직렬화

--- src/io_stream/serial/Person.java
+++ src/io_stream/serial/Person.java
@@ -7,6 +7,10 @@ public class Person implements Serializable {
     // serialVersionUID: 직렬화하여 주고 받을 해당 클래스의 버전
     // 객체를 보낼쪽과 받을 쪽의 명시된 클래스의 내용이 다를 경우를 대비
     // 직접 지정하지 않으면 자동으로 생성
+    // 객체를 직렬화 한 후 UID 를 변경해서 직렬화로 읽어볼 것
+    // -> 예외 발생
+    // 필드가 달라도 시리얼 ID 가 같으면 같은 클래스로 간주
+    // (심각한 데이터 손실 문제 발생 가능성이 있음)
 
     private static final long serialVersionUID = 1;
     private String name;
