414883f2cb52c9c93dad4257901468da9923ff33
kwang-min-lee1 <https://github.com/kwang-min-lee1/sql-study>
2024-04-10 21:52:01 +0900

    변수 선언

--- src/variable/Var1.java
+++ src/variable/Var1.java
@@ -5,7 +5,7 @@ public class Var1 {
 
         // 변수 선언
         int age; // 정수를 저장할 수 있는 age 이름의 변수 선언
-        double value; // 실수를 저장할 수 있는 value라는 이름의 변수 선언
+        double value; // 실수를 저장할 수 있는 value 라는 이름의 변수 선언
 
         int x, y, z; //같은 종류의 변수는 한꺼번에 선언 가능
 
@@ -20,7 +20,7 @@ public class Var1 {
 
         int number = 100; //선언과 초기화 동시에 가능
 
-        int result = sorce +number; // 다른 변수의 값을 불러들여 저장, 또는 연산 가능
+        int result = sorce +number; //  다른 변수의 값을 불러들여 저장, 또는 연산 가능
 
         System.out.println("결과" + result);
 
